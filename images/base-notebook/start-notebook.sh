124439662af9a582d448f92210e7e47978748935
ownEyes <932048437@qq.com>
2023-12-21 22:20:23 +0800

    restructure envs & julia&R modified

--- images/base-notebook/start-notebook.sh
+++ images/base-notebook/start-notebook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Shim to emit warning and call start-notebook.py
 echo "WARNING: Use start-notebook.py instead"
-
+source activate jupyter
 exec /usr/local/bin/start-notebook.py "$@"
--- images/base-notebook/start-singleuser.sh
+++ images/base-notebook/start-singleuser.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Shim to emit warning and call start-singleuser.py
 echo "WARNING: Use start-singleuser.py instead"
-
+# Activate the Conda environment
+source activate jupyter
 exec /usr/local/bin/start-singleuser.py "$@"
